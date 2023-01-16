d1d9b8a10b16a038a6fed141df6607627b45a228
davidbalogh21 <david_balogh21@yahoo.com>
2023-01-16 16:54:12 +0200

    MPA Project

---
+++ Balogh_David_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Balogh_David_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
